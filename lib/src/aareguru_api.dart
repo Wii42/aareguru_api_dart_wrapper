a86f1077efae61d40916d6041328f2d5443d4c6f
Wi42 <101128608+Wii42@users.noreply.github.com>
2024-07-29 21:36:11 +0200

    refactored aareguruApi to use ApiRequest

--- lib/src/aareguru_api.dart
+++ lib/src/aareguru_api.dart
@@ -1,12 +1,8 @@
-import 'dart:convert';
-
-import 'package:aareguru_api/src/response_objects/widget.dart';
-
+import 'api_request.dart';
+import 'response_objects/city.dart';
 import 'response_objects/current/current.dart';
 import 'response_objects/today.dart';
-import 'package:http/http.dart' as http;
-
-import 'response_objects/city.dart';
+import 'response_objects/widget.dart';
 
 /// Wrapper for Aare.guru API v2018 (current)
 class AareGuruApi {
@@ -21,104 +17,37 @@ class AareGuruApi {
 
   AareGuruApi({this.appName, this.appVersion});
 
-  Future<String> _baseRequest(String endpoint,
-      {Map<String, dynamic>? parameters,
-      List<String> values = const []}) async {
-    parameters ??= {};
-    String path = [apiPath, endpoint].join('/');
-    if (appName != null) {
-      parameters['app'] = appName;
-    }
-    if (appVersion != null) {
-      parameters['version'] = appVersion;
-    }
-    if (values.isNotEmpty) {
-      parameters['values'] = values.join(',');
-    }
-    http.Response response = await http.get(Uri.https(host, path, parameters));
-    if (response.statusCode != 200) {
-      throw Exception(
-          'Failed to fetch data from Aare.guru API, status code: ${response.statusCode} ${response.reasonPhrase}');
-    }
-    print(response.request?.url);
-    return response.body;
-  }
+  Future<List<City>> cities() =>
+      CitiesRequest(host, apiPath, appName: appName, appVersion: appVersion)
+          .request();
 
-  Future<String> _cities([List<String>? values]) async {
-    return _baseRequest('cities', values: values ?? []);
-  }
+  Future<List<String>> citiesValues(List<String> values) =>
+      CitiesRequest(host, apiPath, appName: appName, appVersion: appVersion)
+          .requestWithValues(values);
 
-  Future<List<City>> cities() async {
-    String response = await _cities();
-    List<dynamic> cities = jsonDecode(response);
+  Future<Today> today(String city) => TodayRequest(host, apiPath,
+          appName: appName, appVersion: appVersion, city: city)
+      .request();
 
-    return cities.map((city) => City.fromJson(city)).toList();
-  }
+  Future<List<String>> todayValues(String city, List<String> values) =>
+      TodayRequest(host, apiPath,
+              appName: appName, appVersion: appVersion, city: city)
+          .requestWithValues(values);
 
-  Future<List<String>> citiesValues(List<String> values) async {
-    if (values.isEmpty) {
-      throw ArgumentError('Values must not be empty');
-    }
-    String response = await _cities(values);
-    return _parseValues(response);
-  }
+  Future<Current> current(String city) => CurrentRequest(host, apiPath,
+          appName: appName, appVersion: appVersion, city: city)
+      .request();
 
-  Future<String> _today(String city, [List<String>? values]) async {
-    return _baseRequest('today',
-        parameters: {'city': city}, values: values ?? []);
-  }
+  Future<List<String>> currentValues(String city, List<String> values) =>
+      CurrentRequest(host, apiPath,
+              appName: appName, appVersion: appVersion, city: city)
+          .requestWithValues(values);
 
-  Future<Today> today(String city) async {
-    String response = await _today(city);
-    Today today = Today.fromJson(jsonDecode(response));
-    return today;
-  }
+  Future<WidgetData> widget() =>
+      WidgetRequest(host, apiPath, appName: appName, appVersion: appVersion)
+          .request();
 
-  Future<List<String>> todayValues(String city, List<String> values) async {
-    if (values.isEmpty) {
-      throw ArgumentError('Values must not be empty');
-    }
-    String response = await _today(city, values);
-    return _parseValues(response);
-  }
-
-  Future<String> _current(String city, [List<String>? values]) async {
-    return _baseRequest('current',
-        parameters: {'city': city}, values: values ?? []);
-  }
-
-  Future<Current> current(String city) async {
-    String response = await _current(city);
-    Current current = Current.fromJson(jsonDecode(response));
-    return current;
-  }
-
-  Future<List<String>> currentValues(String city, List<String> values) async {
-    if (values.isEmpty) {
-      throw ArgumentError('Values must not be empty');
-    }
-    String response = await _current(city, values);
-    return _parseValues(response);
-  }
-
-  Future<String> _widget([List<String>? values]) async {
-    return _baseRequest('widget', values: values ?? []);
-  }
-
-  Future<WidgetData> widget() async {
-    String response = await _widget();
-    return WidgetData.fromJson(jsonDecode(response));
-  }
-
-  Future<List<String>> widgetValues(List<String> values) async {
-    if (values.isEmpty) {
-      throw ArgumentError('Values must not be empty');
-    }
-    String response = await _widget(values);
-    return _parseValues(response);
-  }
-
-  List<String> _parseValues(String data) {
-    return data.split('\n');
-  }
+  Future<List<String>> widgetValues(List<String> values) =>
+      WidgetRequest(host, apiPath, appName: appName, appVersion: appVersion)
+          .requestWithValues(values);
 }
